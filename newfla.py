13639a7091b3bcef78ff7d6123675046cb2c497b
Szymon Stypa <shabo.o@hotmail.com>
2015-11-25 08:30:54 +0100

    Update 1

--- newfla.py
+++ newfla.py
@@ -13,22 +13,44 @@ def read_packages():
 
 def create_app_structure(name):
     # Create run.py and config.py in the root directory
-    open('run.py', 'w').write("from app import app\napp.run(debug = True)")
-    open('config.py', 'w').write("import os\nbasedir = os.path.abspath(os.path.dirname(__file__))\n\nCSRF_ENABLED = True\nSECRET_KEY = 'superSecret'")
+    with open('run.py', 'w') as f:
+        f.write("from app import app\napp.run(debug = True)")
+        f.close()
+
+    with open('config.py', 'w') as f:
+        f.write("import os\nbasedir = os.path.abspath(os.path.dirname(__file__))\n\nCSRF_ENABLED = True\nSECRET_KEY = 'superSecret'")
+        f.close()
+
     # Create an app directory and enter it
     mkdir("app")
     chdir("app")
+
     # Create views.py and __init__.py in the app directory
-    open('__init__.py', 'w').write("from flask import Flask\nfrom flask.ext.bootstrap import Bootstrap\nfrom flask.ext.moment import Moment\nfrom redis import StrictRedis\n\napp = Flask(__name__)\napp.config.from_object('config')\n\nbootstrap = Bootstrap(app)\nmoment = Moment(app)\nredis = StrictRedis(decode_responses = True)\n\nfrom app import views")
-    open('views.py', 'w').write("from flask import render_template, redirect, url_for, request\nfrom app import app, redis\n\n@app.errorhandler(404)\ndef not_found_error(e):\n    return render_template('404.html', e=e), 404\n\n@app.errorhandler(500)\ndef internal_server_error(e):\n    return render_template('500.html', e=e), 500\n\n@app.route('/')\ndef index():\n    return render_template('index.html')\n")
+    with open('__init__.py', 'w') as f:
+        f.write("from flask import Flask\nfrom flask.ext.bootstrap import Bootstrap\nfrom flask.ext.moment import Moment\nfrom redis import StrictRedis\n\napp = Flask(__name__)\napp.config.from_object('config')\n\nbootstrap = Bootstrap(app)\nmoment = Moment(app)\nredis = StrictRedis(decode_responses = True)\n\nfrom app import views")
+        f.close()
+
+    with open('views.py', 'w') as f:
+        f.write("from flask import render_template, redirect, url_for, request\nfrom app import app, redis\n\n@app.errorhandler(404)\ndef not_found_error(e):\n    return render_template('404.html', e=e), 404\n\n@app.errorhandler(500)\ndef internal_server_error(e):\n    return render_template('500.html', e=e), 500\n\n@app.route('/')\ndef index():\n    return render_template('index.html')\n")
+        f.close()
+
     # Create template directory
     mkdir("templates")
     chdir("templates")
+
     # Create basic html files (404, 500, base and index)
-    open('404.html', 'w').write("{{ e }}")
-    open('500.html', 'w').write("{{ e }}")
-    open('base.html', 'w').write("{% extends 'bootstrap/base.html' %}\n\nv{% block html_attribs %} lang='en' {% endblock %}\n\n{% block head %}\n{{ super() }}\n<title>" + name + "</title>\n{% endblock %}\n\n{% block content %}\n<div class='container'>\n{% block container %}\n{% endblock -%}\n</div>\n{% endblock %}\n\n{% block scripts %}\n{{ super() }}\n{{ moment.include_moment() }}\n{{ moment.lang('en') }}\n{% endblock %}")
-    open('index.html', 'w').write("{% extends 'base.html' %}\n\n{% block container %}\n<h1 class='text-center'>" + name +  "</h1>\n{% endblock %}\n")
+    with open('404.html', 'w') as f:
+        f.write("{{ e }}")
+        f.close()
+    with open('500.html', 'w') as f:
+        f.write("{{ e }}")
+        f.close()
+    with open('base.html', 'w') as f:
+        f.write("{% extends 'bootstrap/base.html' %}\n\nv{% block html_attribs %} lang='en' {% endblock %}\n\n{% block head %}\n{{ super() }}\n<title>" + name + "</title>\n{% endblock %}\n\n{% block content %}\n<div class='container'>\n{% block container %}\n{% endblock -%}\n</div>\n{% endblock %}\n\n{% block scripts %}\n{{ super() }}\n{{ moment.include_moment() }}\n{{ moment.lang('en') }}\n{% endblock %}")
+        f.close()
+    with open('index.html', 'w') as f:
+        f.write("{% extends 'base.html' %}\n\n{% block container %}\n<h1 class='text-center'>" + name +  "</h1>\n{% endblock %}\n")
+        f.close()
 
 @click.command()
 @click.option('--name', default="FlaskApp", help="The name of your project")
